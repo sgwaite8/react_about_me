3d40b3bf5e394b03938f6a189a0014a02eaf838b
Susannah Waite <susannahwaite2@gmail.com>
2016-08-03 18:34:00 -0500

    user container set up with add user method and add user route. Next step set up user form.

--- react_about_me/routes/users.js
+++ react_about_me/routes/users.js
@@ -9,4 +9,15 @@ router.get('/', function(req, res, next) {
   });
 });
 
+//add a user and save them to the database
+router.post('/', function(req, res, next){
+  var name = req.body.name;
+  var newUser = User({name: name});
+
+  newUser.save(function(err, user){
+    if (err) console.log(err);
+    res.json(user);
+  });
+});
+
 module.exports = router;
--- react_about_me/src/components/user-container.js
+++ react_about_me/src/components/user-container.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
 class UserContainer extends React.Component {
-
+  constructor(){
+    super();
+    this.state = {users: []};
+  }
+  componentDidMount(){
+    $.ajax({
+      method: 'GET',
+      url: '/users',
+      dataType: 'json'
+    }) .done(function(data){
+      this.setState({users: data})
+    }.bind(this))
+  }
+  _addUser(newUser){
+    let currentUsers = this.state.users;
+    currentUsers.push(newUser);
+    this.setState({users: currentUsers});
+  }
+  render(){
+    return (
+      <div>
+        <UserForm addUser={ this._addUser.bind(this) } />
+      </div>
+    )
+  }
 }
 
 export default UserContainer;
